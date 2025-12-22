d72b97da0b4998af88608cda81589c3b5fdd6415
alessandrofelder <a.felder@ucl.ac.uk>
2025-12-22 12:25:23 +0100

    address review comments

--- crab/configure_slurm_crab.sh
+++ crab/configure_slurm_crab.sh
@@ -9,18 +9,19 @@
 #SBATCH -o slurm.%x.%N.%j.out # write STDOUT
 #SBATCH -e slurm.%x.%N.%j.err # write STDERR
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=a.felder@ucl.ac.uk
+#SBATCH --mail-user=<name@email.com>
 
 # Load the required modules
 module load /ceph/neuroinformatics/neuroinformatics/modules/modulefiles/template-builder/2024-12-02
 
+# Set up QBATCH variables to configure parallel jobs that will be spawned by modelbuild.sh
 export QBATCH_PPJ=5
 export QBATCH_CHUNKSIZE=1
 export QBATCH_CORES=1
 export QBATCH_SYSTEM="slurm"
 export QBATCH_QUEUE="cpu"
 export QBATCH_MEM="32G"
-export QBATCH_OPTIONS="--mail-type=ALL --mail-user=a.felder@ucl.ac.uk --mem 32G"
+export QBATCH_OPTIONS="--mail-type=ALL --mail-user=<name@email.com> --mem 32G"
 
 # Define atlas-forge directory, species and template names, and average type
 ATLAS_DIR="/ceph/neuroinformatics/neuroinformatics/atlas-forge"
@@ -30,7 +31,6 @@ AVE_TYPE="mean"
 
 TEMPLATE_DIR="${ATLAS_DIR}/${SPECIES}/templates/${TEMP_NAME}/"
 
-# Verify that the working directory exists before changing directory
 if [ ! -d "${TEMPLATE_DIR}" ]; then
   mkdir $TEMPLATE_DIR
   echo "Created new template directory ${TEMPLATE_DIR}"
@@ -39,13 +39,13 @@ else
 fi
 
 # log the script to the output folder for traceability
-cp $0 "${TEMPLATE_DIR}/configure_script.txt"
+cp $0 "${TEMPLATE_DIR}/slurm_configuration_script_log.txt"
 
 # And give whole NIU group read permission
-chmod g+r "${TEMPLATE_DIR}/configure_script.txt"
+chmod g+r "${TEMPLATE_DIR}/slurm_configuration_script_log.txt"
 
 # Path to the bash script that builds the template
-BUILD_SCRIPT="${ATLAS_DIR}/build_slurm.sh"
+BUILD_SCRIPT="${ATLAS_DIR}/build_template_with_slurm.sh"
 
 if [ ! -f $BUILD_SCRIPT ]; then
   echo "Error: ${BUILD_SCRIPT} does not exist."
