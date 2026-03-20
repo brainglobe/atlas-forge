af01a600ee8453554015bbfad471cf064569cf72
alessandrofelder <alessandrofelder@users.noreply.github.com>
2026-03-20 16:07:15 +0000

    high-res draft zebrafinch template

---
+++ zebrafinch/standardise_zebrafinch.py
@@ -0,0 +1,8 @@
+from brainglobe_template_builder.standardise import standardise
+
+from pathlib import Path
+if __name__ == "__main__":
+    source_csv = Path("/media/ceph/margrie/sweiler/RawData/Tracing_Imaging/serial2p/Bird_brains/zebra_finch_atlas/2026-03-18_zebrafinch.csv")
+    output_dir = Path("/media/ceph/neuroinformatics/neuroinformatics/atlas-forge/ZebraFinch/")
+    output_vox_size = 25
+    standardise(source_csv=source_csv, output_dir=output_dir, output_vox_size=output_vox_size)
