b0994fae0d3798af873662dc04322591baf51db4
alessandrofelder <alessandrofelder@users.noreply.github.com>
2026-05-08 16:40:12 +0100

    fix typo in comment

--- crab/preprocess_crab.py
+++ crab/preprocess_crab.py
@@ -5,5 +5,5 @@ from pathlib import Path
 
 if __name__ == "__main__":
     output_dir = Path("/home/alessandro/crab_atlas_forge")
-    config = PreprocConfig(output_dir=output_dir, mask=MaskConfig(gaussian_sigma=6, closing_size=10))  # couble defaults for double-resolution
+    config = PreprocConfig(output_dir=output_dir, mask=MaskConfig(gaussian_sigma=6, closing_size=10))  # double defaults for double-resolution
     preprocess(output_dir/"standardised/standardised_images.csv", config=config)
